a70ec84e51d0ffdf9d2bb24af935219fd2398a0b
FrogWithAGun <139518149+FrogWithAGun@users.noreply.github.com>
2023-09-28 12:44:21 +1000

    Add files via upload

---
+++ vue-project/src/App.vue
@@ -0,0 +1,49 @@
+<script setup>
+  import { RouterLink, RouterView } from 'vue-router'
+  import { ref } from 'vue'
+  import ButtonCounter from '../src/components/ButtonCounter.vue'
+
+  const menuVisible = ref(false);
+
+  // A function that changes menu visibility as well as the text in the button that activates it
+  const changeMenuVisibility = () => {
+    menuVisible.value = !menuVisible.value;
+    
+    if (menuVisible.value == false) {
+      document.getElementById('visibility-button').innerText = "Show Menu";
+    }
+
+    else {
+      document.getElementById('visibility-button').innerText = "Hide Menu";
+    }
+  }
+</script>
+
+<template>
+  <div class="page-content">
+    <div class="menu-container">
+      <div class="button-container">
+        <button @click="changeMenuVisibility" id="visibility-button" class="nice-button">Show Menu</button>
+      </div>
+
+      <br>
+
+      <div v-if="menuVisible" class="links-container">
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/projects">View Projects</RouterLink>
+        <RouterLink to="/about">About Me</RouterLink>
+      </div>
+    </div>
+
+    <div class="section-content">
+      <RouterView></RouterView>
+    </div>
+
+
+    <br><br>
+
+    <div class="button-game-container">
+      <ButtonCounter></ButtonCounter>
+    </div>
+  </div>
+</template>
